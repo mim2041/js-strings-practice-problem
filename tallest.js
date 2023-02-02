5547cecb0679241190117a231a3614299e9aa1c4
mim2041 <suraiyamim338@gmail.com>
2023-02-02 11:36:59 +0600

    math function finding max and min

---
+++ tallest.js
@@ -0,0 +1,17 @@
+function maxArray(numbers){
+    let largest = numbers[0];
+    for(let i=0; i<numbers.length; i++){
+        
+        const index = i;
+        const element = numbers[index];
+        // console.log(element);
+        if(element > largest){
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+const heights = [167, 190, 120, 165, 137];
+const tallest = maxArray(heights);
+console.log('Tallest person is', tallest);
